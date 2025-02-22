3d0c2923b06ab47dddb89ff51e5073ea1dd8cbf5
Ryan Provenzano <ryan.provenz@gmail.com>
2025-02-21 23:08:52 -0500

    refactored Scene into a class and put player into one of the scene's properties

---
+++ src/TextCluster.js
@@ -0,0 +1,11 @@
+class TextCluster extends Phaser.GameObjects.BitmapText
+{
+    constructor (scene, x, y)
+    {
+        super(scene, x, y, 'textCluster');
+
+        this.setScale(0.5);
+    }
+
+    
+}
